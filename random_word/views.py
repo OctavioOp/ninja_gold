edd19639f3676c3774f1904e006f59ab3ef47606
Octavio <oc.opazo@outlook.com>
2021-07-29 19:46:37 -0400

    practica con sesiones en django

---
+++ random_word/views.py
@@ -0,0 +1,36 @@
+from typing import Counter
+from django.shortcuts import redirect, render, HttpResponse
+from django.utils.crypto import get_random_string
+
+
+# Create your views here.
+
+def home(request):
+    counter = request.session.get('counter')
+    
+    if 'counter' not in request.session:
+        request.session['counter'] = 0
+
+    request.session['counter'] += 1
+
+    word = get_random_string(length=14)
+
+    context = {
+        'word': word,
+        'counter': counter
+        
+    }
+    return render(request,'index.html',context)
+
+
+def login(request):
+    request.session['user'] = 'user'
+    request.session['counter']= 0
+    return redirect('../random/home')
+
+def logout(request):
+    del request.session['user']
+    del request.session['counter']
+    return redirect('../random/login')
+
+
